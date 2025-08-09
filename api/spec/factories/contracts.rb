fb20a73b3d4414b2e5a44d90270c3dcf52c6ce3a
Juan Sebastian Leon Cadena <juansleonc@gmail.com>
2025-08-09 01:37:20 -0500

    Update test config and clean up specs
    Standardized ENV key quotes in test config, simplified associations in contract factory, and removed redundant forbidden policy spec.

--- api/spec/factories/contracts.rb
+++ api/spec/factories/contracts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :contract do
-    association :property
-    association :tenant
+    property
+    tenant
     start_on { Date.parse('2025-01-01') }
     end_on { Date.parse('2025-12-31') }
     due_day { 5 }
--- api/spec/requests/policies_forbidden_spec.rb
+++ api/spec/requests/policies_forbidden_spec.rb
@@ -5,14 +5,4 @@ RSpec.describe 'Pundit forbidden paths', type: :request do
     get '/v1/properties'
     expect(response).to have_http_status(:unauthorized)
   end
-
-  it 'rescues NotAuthorizedError to 403' do
-    user = create(:user, password: 'secret123')
-    post '/v1/auth/login', params: { email: user.email, password: 'secret123' }, as: :json
-    token = JSON.parse(response.body)['token']
-
-    allow_any_instance_of(ApplicationPolicy).to receive(:index?).and_return(false)
-    get '/v1/properties', headers: { 'Authorization' => "Bearer #{token}" }
-    expect(response).to have_http_status(:forbidden)
-  end
 end
