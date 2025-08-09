2ccaf16f0e2cdef864b13d109bdee7ebeba1f7e3
Juan Sebastian Leon Cadena <juansleonc@gmail.com>
2025-08-09 00:30:44 -0500

    fix(test): disable SimpleCov during rswag swaggerize dry-run
    - Guard coverage start when formatter is Rswag or --dry-run is used

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -1,12 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'simplecov'
-SimpleCov.start 'rails' do
-  enable_coverage :branch
-  minimum_coverage 100
-  minimum_coverage_by_file 100
-  add_filter '/app/controllers/application_controller.rb'
-  add_filter '/app/policies/application_policy.rb'
+
+# Disable coverage when generating Swagger (rswag dry-run) to avoid false failures
+SWAGGER_MODE = ENV['RSWAG'] == 'true' || ARGV.any? { |a| a.include?('Rswag::Specs::SwaggerFormatter') || a == '--dry-run' }
+unless SWAGGER_MODE
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    enable_coverage :branch
+    minimum_coverage 100
+    minimum_coverage_by_file 100
+    add_filter '/app/controllers/application_controller.rb'
+    add_filter '/app/policies/application_policy.rb'
+  end
 end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
