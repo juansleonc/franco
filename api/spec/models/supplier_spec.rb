7a3011a37e1eac3af614671c1ec99520b26c746b
Juan Sebastian Leon Cadena <juansleonc@gmail.com>
2025-08-09 00:14:28 -0500

    style(test): rubocop -A autocorrections in model specs
    - Use described_class and be_valid matchers

--- api/spec/models/supplier_spec.rb
+++ api/spec/models/supplier_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
   it "validates presence of name and tax_id" do
-    supplier = Supplier.new
-    expect(supplier.valid?).to be_falsey
+    supplier = described_class.new
+    expect(supplier).not_to be_valid
     expect(supplier.errors[:name]).to be_present
     expect(supplier.errors[:tax_id]).to be_present
   end
@@ -11,8 +11,8 @@ RSpec.describe Supplier, type: :model do
   it "validates uniqueness of tax_id" do
     user = create(:user)
     create(:supplier, tax_id: "X1", created_by_user: user)
-    dup = Supplier.new(name: "Dup", tax_id: "X1", email: "a@b.com", created_by_user: user)
-    expect(dup.valid?).to be_falsey
+    dup = described_class.new(name: "Dup", tax_id: "X1", email: "a@b.com", created_by_user: user)
+    expect(dup).not_to be_valid
     expect(dup.errors[:tax_id]).to be_present
   end
 end
--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid with email and password" do
-    user = User.new(email: "a@b.com", password: "Password123!", password_confirmation: "Password123!")
-    expect(user.valid?).to be_truthy
+    user = described_class.new(email: "a@b.com", password: "Password123!", password_confirmation: "Password123!")
+    expect(user).to be_valid
   end
 
   it "requires email" do
-    user = User.new(password: "Password123!", password_confirmation: "Password123!")
-    expect(user.valid?).to be_falsey
+    user = described_class.new(password: "Password123!", password_confirmation: "Password123!")
+    expect(user).not_to be_valid
     expect(user.errors[:email]).to be_present
   end
 end
